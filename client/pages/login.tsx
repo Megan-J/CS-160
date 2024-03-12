e42f847dc1bd714601a3892f5772e7af06da6e87
Megan-J <mmju2103@gmail.com>
2024-03-11 22:45:24 -0700

    new login UI

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -3,31 +3,46 @@ import React, { useState, useEffect } from "react";
 function login() {
   const [message, setMessage] = useState("Loading");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/home")
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-  }, []);
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md">
-      <nav className="main">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="stores">Stores</a></li>
-                <li><a href="upload">Upload Music</a></li>
-                <li><a href="login">Login</a></li>
-                <li><a href="signup">Signup</a></li>
-            </ul>
+        <nav className="main">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="stores">Stores</a>
+            </li>
+            <li>
+              <a href="upload">Upload Music</a>
+            </li>
+            <li>
+              <a href="login">Login</a>
+            </li>
+            <li>
+              <a href="signup">Signup</a>
+            </li>
+          </ul>
         </nav>
-        <h1 className="text-2xl font-semibold mb-4">CloudSound</h1>
-        <p>Login</p>
-
-        <p>{message}</p>
+        <h1 className="text-2xl font-semibold mb-4">Radar</h1>
+        <div>
+          <h2 className="text-xl mb-4">Login</h2>
+          <form>
+            <label className="font-semibold">
+              Username
+              <input type="text" name="username" />
+            </label>
+            <label className="font-semibold">
+              Password
+              <input type="text" name="password" />
+            </label>
+            <br />
+            <button type="button">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
-    
   );
 }
 
--- client/pages/upload.tsx
+++ client/pages/upload.tsx
@@ -4,32 +4,35 @@ import React, { useState, useEffect } from "react";
 function upload() {
   const [message, setMessage] = useState("Loading");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/home")
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-  }, []);
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md">
-      <nav className="main">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="stores">Stores</a></li>
-                <li><a href="upload">Upload Music</a></li>
-                <li><a href="login">Login</a></li>
-                <li><a href="signup">Signup</a></li>
-            </ul>
+        <nav className="main">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="stores">Stores</a>
+            </li>
+            <li>
+              <a href="upload">Upload Music</a>
+            </li>
+            <li>
+              <a href="login">Login</a>
+            </li>
+            <li>
+              <a href="signup">Signup</a>
+            </li>
+          </ul>
         </nav>
-        
+
         <h1 className="text-2xl font-semibold mb-4">Upload Music</h1>
         <p>Let the world hear your magic!</p>
 
         <p>{message}</p>
       </div>
     </div>
-    
   );
 }
 
