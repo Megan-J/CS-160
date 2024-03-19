2abf72d4a1086278a712c1cfe223564766cecb29
Megan-J <mmju2103@gmail.com>
2024-03-18 18:28:19 -0700

    finished forms

--- client/pages/checkout.tsx
+++ client/pages/checkout.tsx
@@ -50,14 +50,12 @@ export default function placeOrder() {
         <div>
           <div>
             <h2 className="text-xl mb-4">Order</h2>
-
             <div>
-              <form>
+              <form className="contact">
                 <a className="font-semibold">Contact</a>
                 <br />
                 <label>
-                  Email
-                  <input type="text" placeholder="email" name="email" />
+                  <input type="text" placeholder="Email" name="email" />
                   <br />
                   <br />
                 </label>
@@ -71,27 +69,144 @@ export default function placeOrder() {
                 </label>
                 <br />
                 <label>
-                  Email
-                  <input type="text" name="email" />
+                  <input type="text" placeholder="Email" name="email" />
                 </label>
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      name="firstName"
+                    />
+                  </label>
+
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      name="lastName"
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input type="text" placeholder="Address" name="address" />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input type="text" placeholder="City" name="city" />
+                  </label>
+                  <label>
+                    <input type="text" placeholder="State" name="state" />
+                  </label>
+                  <label>
+                    <input type="text" placeholder="ZIP Code" name="zip" />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Phone Number"
+                      name="phoneNumber"
+                    />
+                  </label>
+                </p>
+                <br />
+                <br />
+                <a className="font-semibold">Payment</a>
+                <br />
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Card Number"
+                      name="cardNumber"
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Name on Card"
+                      name="nameOnCard"
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Expiration date (MM/YY)"
+                      name="expirationDate"
+                    />
+                  </label>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Security Code"
+                      name="securityCode"
+                    />
+                  </label>
+                </p>
+                <br />
+                <br />
+                <a className="font-semibold">Billing Address</a>
+                <br />
                 <label>
-                  First name
-                  <input type="text" name="email" />
-                </label>
-                <label>
-                  Last name
-                  <input type="text" name="email" />
-                </label>
-                <label>
-                  Address
-                  <input type="text" name="email" />
-                </label>
-                <label>
-                  City
-                  <input type="text" name="password" />
+                  Country/Region
+                  <select>
+                    <option value={"United States"}>United States</option>
+                  </select>
                 </label>
                 <br />
+                <label>
+                  <input type="text" placeholder="Email" name="email" />
+                </label>
+                <p>
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      name="firstName"
+                    />
+                  </label>
+
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      name="lastName"
+                    />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input type="text" placeholder="Address" name="address" />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input type="text" placeholder="City" name="city" />
+                  </label>
+                  <label>
+                    <input type="text" placeholder="State" name="state" />
+                  </label>
+                  <label>
+                    <input type="text" placeholder="ZIP Code" name="zip" />
+                  </label>
+                </p>
+                <button onClick={handleSubmit}>Confirm Order</button>
               </form>
+              <br />
+              <br />
+              <br />
+              <a>
+                By placing your order you agree to Terms and Conditions, Privacy
+                Notice, and Cookie Policy.
+              </a>
             </div>
           </div>
         </div>
