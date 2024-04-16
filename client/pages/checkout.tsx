ee42e2dfd2d7b3c70bcdd1885ddc6b8ba39ddbac
Megan-J <mmju2103@gmail.com>
2024-04-16 11:37:51 -0700

    added front for API

--- client/pages/checkout.tsx
+++ client/pages/checkout.tsx
@@ -19,6 +19,12 @@ interface Inputs {
 }
 
 export default function placeOrder() {
+  require("dotenv").config({ debug: true });
+  //console.log(process.env);
+
+  let applicationKey = process.env.SQUARE_API_APPLICATION_KEY;
+  let locationKey = process.env.SQUARE_API_LOCATION_KEY;
+
   const [inputs, setInputs] = useState<Inputs>({});
 
   const dummyOrder = {
@@ -61,20 +67,6 @@ export default function placeOrder() {
   return (
     <Panel title="Checkout">
       <h2 className="text-xl mb-4">Order</h2>
-
-      <div className="payment-form">
-        <PaymentForm
-          applicationId={process.env.SQUARE_API_APPLICATION_KEY}
-          cardTokenizeResponseReceived={async (token, verifiedBuyer) => {
-            console.log("token:", token);
-            console.log("verifiedBuyer:", verifiedBuyer);
-          }}
-          locationId={process.env.SQUARE_API_LOCATION_KEY}
-        >
-          <CreditCard />
-        </PaymentForm>
-      </div>
-
       <div>
         <ul>
           {dummyOrder.items.map((item) => (
@@ -184,94 +176,18 @@ export default function placeOrder() {
               />
             </label>
           </p>
-          <br />
-          <br />
-          <a className="font-semibold">Payment</a>
-          <br />
-          <p>
-            <label>
-              <input
-                type="text"
-                placeholder="Card Number"
-                name="cardNumber"
-                value={inputs.cardNumber || ""}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                type="text"
-                placeholder="Name on Card"
-                name="nameOnCard"
-                value={inputs.nameOnCard || ""}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                type="text"
-                placeholder="Expiration date (MM/YY)"
-                name="expiration"
-                value={inputs.expiration || ""}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              <input
-                type="text"
-                placeholder="Security Code"
-                name="securityCode"
-                value={inputs.securityCode || ""}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <br />
-          <br />
-          <a className="font-semibold">Billing Address</a>
-          <br />
-          <label>
-            Country/Region
-            <select>
-              <option value={"United States"}>United States</option>
-            </select>
-          </label>
-          <br />
-          <label>
-            <input type="text" placeholder="Email" name="email" />
-          </label>
-          <p>
-            <label>
-              <input type="text" placeholder="First Name" name="firstName" />
-            </label>
-
-            <label>
-              <input type="text" placeholder="Last Name" name="lastName" />
-            </label>
-          </p>
-          <p>
-            <label>
-              <input type="text" placeholder="Address" name="address" />
-            </label>
-          </p>
-          <p>
-            <label>
-              <input type="text" placeholder="City" name="city" />
-            </label>
-            <label>
-              <input type="text" placeholder="State" name="state" />
-            </label>
-            <label>
-              <input type="text" placeholder="ZIP Code" name="zip" />
-            </label>
-          </p>
-          <br />
+          <div className="payment-form">
+            <PaymentForm
+              applicationId={applicationKey}
+              cardTokenizeResponseReceived={async (token, verifiedBuyer) => {
+                console.log("token:", token);
+                console.log("verifiedBuyer:", verifiedBuyer);
+              }}
+              locationId={locationKey}
+            >
+              <CreditCard />
+            </PaymentForm>
+          </div>
           <button onClick={handleSubmit}>Confirm Order</button>
         </form>
         <br />
